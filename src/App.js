48dabb7a6d3ea600b49ab261006d6a17c8977719
Dusty211 <kylebenz@hotmail.com>
2019-04-06 14:59:50 -0400

    finished basic setup of material-ui

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import ArtPane from './containers/ArtPane.js'
 import DialogPane from './containers/DialogPane.js'
 import InfoPane from './containers/InfoPane.js'
 import ActionPane from './containers/ActionPane.js'
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        < Header />
-        < ArtPane />
-        < DialogPane />
-        < InfoPane />
-        < ActionPane />
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <div>
+          < Header />
+          < ArtPane />
+          < DialogPane />
+          < InfoPane />
+          < ActionPane />
+        </div>
+      </ React.Fragment>
     );
   }
 }
