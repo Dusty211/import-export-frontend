e1ff89ceccfb463e0a5fa1fe9f62b66311091fd9
Dusty211 <kylebenz@hotmail.com>
2019-04-15 22:30:16 -0400

    greeting when now saved games

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -3,11 +3,17 @@ import GameSave from './GameSave.js'
 
 const GamesList = (props) => {
   return(
-<div>
-{props.user.gamestates.map((gamestate) => {
-  return <GameSave key={gamestate.id} gamestate={gamestate} setCurrentGame={props.setCurrentGame} />
-})}
-</div>
+<React.Fragment>
+  {props.user.gamestates.length === 0 ?
+    <div>
+      <h1>{`You have no saved games!`}</h1>
+      <h3>{`Select "New Game" to start a game.`}</h3>
+    </div> :
+      props.user.gamestates.map((gamestate) => {
+      return <GameSave key={gamestate.id} gamestate={gamestate} setCurrentGame={props.setCurrentGame} />
+    })
+  }
+</React.Fragment>
   )
 }
 
